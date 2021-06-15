5b37e45fe775043fa21b4cf22d832101ca75e024
Tony Gyan <49129216+TONYOG12@users.noreply.github.com>
2021-06-15 22:42:45 +0000

    Update arduino code.cpp

--- src/arduino code.cpp
+++ src/arduino code.cpp
@@ -3,17 +3,19 @@ int pirsensor2 = 0;
 
 void setup()
 {
+  //setup code which runs once
   pinMode(13, OUTPUT);
   pinMode(2, INPUT);
   pinMode(4, INPUT);
  
 }
-
+//counts the number of people who have entered the room through the entrance
 int count = 0;
+//counts the number of people who have left the room through the exit
 int count2 = 0;
 void loop()
 {
-  
+  //main code which runs repeatedly
   pirsensor = digitalRead(2);
   pirsensor2 = digitalRead(4);
   
@@ -24,7 +26,7 @@ void loop()
   }
   
   if(pirsensor2 == HIGH)
-  {	
+  {	 
      count2++;
      if(count - count2 == 0)
      {	
